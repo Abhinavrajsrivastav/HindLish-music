a3b81c22f071b17cacdb4709ed055127a38a8e9b
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-03-07 00:22:09 +0530

    first commit

---
+++ src/playlist.js
@@ -0,0 +1,138 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Loki Epic",
+      cover:
+        "images/one-direction.jpg",
+      artist: "Makzo",
+      audio: "/musics/one-direction.mp3",
+      color: ["#99BC85", "#12372A"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Glorious Purpose",
+      cover:
+        "images/Love.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "/musics/photo_graph.mp3",
+      color: ["#E1F0DA", "#643843"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Legends",
+      cover:
+        "images/legends.jpg",
+      artist: "Aiguille",
+      audio: "/musics/legends.mp3",
+      color: ["#C9D7DD", "#7D0A0A"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Hall of fame",
+      cover:
+        "images/Champian.jpg",
+      artist: "Swørn",
+      audio: "/musics/The_Script_-_Hall_Of_Fame_Lyrics_(getmp3.pro).mp3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Coldplay",
+      cover:
+        "images/ColdPlay.jpg",
+      artist: "Swørn",
+      audio: "musics/The_Chainsmokers_Coldplay_-_Somet_(getmp3.pro).mp3",
+      color: ["#335561", "#3887BE"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Shineing in the shadow",
+      cover:
+        "images/TeraHoneLagaHu.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "musics/Tera_Hone_Laga_Hoon_Lyrical_-_Ajab_(getmp3.pro).mp3",
+      color: ["#2A416D", "#FF9800"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Subhanallah",
+      cover:
+        "images/SubhanAllah.jpg",
+      artist: "Swørn",
+      audio: "musics/Subhanallah_-_Yeh_Jawaani_Hai_Deewa_(getmp3.pro).mp3",
+      color: ["#FDBF60", "#191919"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mere naam tu",
+      cover:
+        "images/MereNaamTu.jpg",
+      artist: "Strehlow, Aylior",
+      audio: "musics/Mere_Naam_Tu_From_Zero_(getmp3.pro).mp3",
+      color: ["#FEFBF6", "#BE5853"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Aditi",
+      cover:
+        "images/Aditi.jpg",
+      artist: "Cloudchord, G Mills",
+      audio: "musics/Lyrical_Kabhi_Kabhi_Aditi_Zindagi_(getmp3.pro).mp3",
+      color: ["#FF194A", "#41B3B3"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Glorious Purpose",
+      cover:
+        "images/Beautiful.jpg",
+      artist: "Philanthrope",
+      audio: "musics/beautiful_people.mp3",
+      color: ["#4CB9E7","#E36414"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ilaahi",
+      cover:
+            "images/Illahi.jpg",
+      artist: "Illahi",
+      audio: "musics/Illahi_-_Yeh_Jawaani_Hai_Deewani__(getmp3.pro).mp3",
+      color: ["#FF9130", "#B15EFF"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Let me love you",
+      cover:
+        "images/LetMeLoveYou.jpg",
+      artist: "Swørn",
+      audio: "musics/DJ_Snake_-_Let_Me_Love_You_ft_Just_(getmp3.pro).mp3",
+      color: ["#00A9FF", "#3A4D39"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Challa",
+      cover:
+        "images/Challah.jpg",
+      artist: "Hanz",
+      audio: "musics/Challa_Full_Song_Jab_Tak_Hai_Ja_(getmp3.pro).mp3",
+      color: ["#90B4AA", "#F2AE87"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
